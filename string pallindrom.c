74c89d33751a9a36c8c496cacef3bed3fc9e142e
samartha17 <59867205+samartha17@users.noreply.github.com>
2020-01-20 21:01:06 +0530

    Add files via upload

---
+++ string pallindrom.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void conc(char c[100]);
+void main()
+{
+char c[100];
+scanf("%s",c);
+conc(c);
+}
+void conc(char c[100])
+{
+char s[100];
+int len,i,j;
+len=strlen(c);
+for(i=len,j=0; i>=0,j<=len; i--,j++)
+{
+s[j]=c[i];
+}
+printf("%s",s);
+if(s==c)
+printf("pallindrom");
+else
+printf("not pallindrom");
+
+}
